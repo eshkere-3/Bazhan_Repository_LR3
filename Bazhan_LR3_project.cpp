d45e7aff65bfc10af939f9819568d4771e0df17a
eshkere-3 <6092listik@gmail.com>
2025-03-31 13:03:42 +0300

    Create void main

---
+++ Bazhan_LR3_project.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double enter_A() {
+
+    //коооооооооод
+
+
+}
+
+double enter_B() {
+
+    //коооооооооод
+
+}
+
+double proizv(int A, int B) {
+
+    //коооооооооод
+
+}
+
+double delen(int A, int B) {
+
+
+    //коооооооооод
+
+}
+
+int main() {
+
+    cout << "Task 1. Ввести А. ";
+    cout << "Task 2. Ввести В. ";
+    cout << "Task 3. Выполнить операцию “*”. ";
+    cout << "Task 4. Выполнить операцию “/”. ";
+    cout << "";
+}
